6318937e049ab8778b961d292f31350be20de824
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-04-25 16:54:42 +0530

    some changes

--- keyboards/handwired/dactyl_promicro/keymaps/adgai/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/adgai/keymap.c
@@ -14,7 +14,7 @@
 #define ENT LSFT_T(KC_ENT)
 #define SPC LALT_T(KC_SPC)
 #define S_TAB S(KC_TAB)
-#define CTRL LCTL_T(KC_DEL)
+#define CTRL LCTL_T(KC_ESC)
 #define GUI LGUI_T(KC_BSPC)
 #define L2_BSPC LCTL(KC_BSPC)
 #define S_GUI LSFT(KC_LGUI)
@@ -30,6 +30,10 @@
 
 #define HT LCTRL_T(KC_H)
 #define N  RCTRL_T(KC_E)
+
+#define SF2 LCTL(LALT(KC_F2))
+#define SF7 LCTL(LALT(KC_F7))
+#define SCROT LWIN(LSFT(KC_S))
 
 // Repeat key, stolen from https://gist.github.com/NotGate/3e3d8ab81300a86522b2c2549f99b131
 enum custom_keycodes {
@@ -48,16 +52,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 CTRL,              SPC,
                                                                 TG(_QWERTY) ,      TG(_LOWER)
     ),
+
     [_QWERTY] = LAYOUT_6x6(
-        KC_GRV , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-        KC_TAB, KC_Q  ,KC_W   , KC_F  , KC_P  , KC_B  ,                                        KC_J   , KC_L  , KC_U  , KC_Y  , KC_SCLN  ,KC_MINS,
-        KC_ESC, KC_A  ,KC_R   , KC_S  , KC_T  , KC_G  ,                                        KC_M   , KC_N  , KC_E  , KC_I  ,KC_O,KC_QUOT,
-        KC_LSFT, KC_Z ,KC_X   , KC_C  , KC_D  , KC_V  ,                                        KC_K   , KC_H  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
-        KC_EQL,KC_QUOT,ALT_SHFT,KC_LEFT,KC_RGHT,KC_SPC ,                                       ENT , KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,_______,
-                                                OSL(_LOWER),KC_LSFT,KC_LGUI,       ENT,KC_LALT,TG(_RAISE) ,
-                                                                KC_LEFT_CTRL,        SPC,
-                                                                _______,        TG(_LOWER)
+        KC_EQL , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_DEL , KC_Q  ,KC_W   , KC_E  , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        KC_BSPC, KC_A  ,KC_S   , KC_D  , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LSFT, KC_Z ,KC_X   , KC_C  , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+        KC_GRV,KC_QUOT,ALT_SHFT,KC_LEFT,KC_RGHT,KC_SPC ,                                       KC_ENT , KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,TG(_WORKMAN),
+                                                OSL(_LOWER),KC_LSFT,GUI,       ENT,REPEAT,TG(_RAISE) ,
+                                                                CTRL,              SPC,
+                                                                TG(_QWERTY) ,      TG(_LOWER)
     ),
+    // [_QWERTY] = LAYOUT_6x6(
+    //     KC_GRV , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+    //     KC_TAB, KC_Q  ,KC_W   , KC_F  , KC_P  , KC_B  ,                                        KC_J   , KC_L  , KC_U  , KC_Y  , KC_SCLN  ,KC_MINS,
+    //     KC_ESC, KC_A  ,KC_R   , KC_S  , KC_T  , KC_G  ,                                        KC_M   , KC_N  , KC_E  , KC_I  ,KC_O,KC_QUOT,
+    //     KC_LSFT, KC_Z ,KC_X   , KC_C  , KC_D  , KC_V  ,                                        KC_K   , KC_H  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+    //     KC_EQL,KC_QUOT,ALT_SHFT,KC_LEFT,KC_RGHT,KC_SPC ,                                       ENT , KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,_______,
+    //                                             OSL(_LOWER),KC_LSFT,KC_LGUI,       ENT,KC_LALT,TG(_RAISE) ,
+    //                                                             KC_LEFT_CTRL,        SPC,
+    //                                                             _______,        TG(_LOWER)
+    // ),
     [_LOWER] = LAYOUT_6x6(
         _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
         KC_COLN,KC_EXLM,KC_AT  ,KC_LBRC,KC_RBRC,KC_PIPE,                                       KC_EQL  ,KC_7  ,KC_8  ,KC_9  ,KC_ASTR,KC_F12 ,
@@ -69,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 _______,       _______
     ),
     [_RAISE] = LAYOUT_6x6(
-        _______,_______,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
+        SF2    ,SF7    ,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
         _______,_______,KC_WH_U	,KC_MS_U,KC_WH_D	,_______,                                       _______,KC_TAB,KC_UP,S_TAB,_______,_______,
-        _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                                       KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_MPLY,_______,
+        SCROT,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                                       _______,KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,_______,
         _______,_______,_______,KC_BTN1,KC_BTN2,_______,                                       _______,_______,KC_MPRV,KC_MNXT,_______,_______,
         QK_BOOT  ,_______,_______,_______,_______,_______,                                       KC_WBAK,KC_VOLU,KC_VOLD,KC_MUTE,_______,_______,
                                                 _______,_______,_______,       _______,_______,_______,
